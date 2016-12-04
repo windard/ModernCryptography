61303e924fe00dd852076c698af7891ff1d4ec1f
Yang Wenqiang <1106911190@qq.com>
2016-12-04 23:50:02 +0800

    Revert "add"
    This reverts commit 00fd39b09b5621ff84096cca063ab2a609061fef.

--- cryptopals/quiz2.py
+++ cryptopals/quiz2.py
@@ -18,13 +18,3 @@ def strxor(a, b):     # xor two strings of different lengths
        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
     else:
        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
-
-
-def hex2bin(a):
-	return "".join(["%04d"%(int(bin(int(x,16))[2:])) for x in a])
-
-def hex2xor2bin(a):
-	return "".join(["%04d"%(int(bin(15^int(x,16))[2:])) for x in a])
-
-print hex2bin("ab24bd2a1b7ac3")
-print hex2xor2bin("ab24bd2a1b7ac3")
